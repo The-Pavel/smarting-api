6312b18204581623a408b4cd355f30442b25e4b5
Pavel Liser <liser.pavel@gmail.com>
2019-04-25 11:25:51 +0800

    added create route and method for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -2,4 +2,24 @@ class Api::V1::PostsController < Api::V1::BaseController
   def index
     @posts = Post.all
   end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      render :index, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :content)
+  end
+
+  def render_error
+    render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
+  end
+
 end

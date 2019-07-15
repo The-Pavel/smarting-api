7fe2ef50fed7436fff0ea00b8f89a082e7030569
Pavel Liser <liser.pavel@gmail.com>
2019-07-15 13:25:33 +0800

    fixed dependent destroy with background selfdestruct job

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,10 @@
 class Post < ApplicationRecord
-  has_many :comments, dependent: :delete_all
+  has_many :comments, dependent: :destroy
   after_save :self_destruct
 
   private
 
   def self_destruct
-    TerminateJob.set(wait: 12.hour).perform_later(self.id)
+    TerminateJob.set(wait: 10.hours).perform_later(self.id)
   end
 end

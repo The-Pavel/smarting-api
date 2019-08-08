072ff38df0c2d21df629f54392e13c911abd2b0c
Pavel Liser <liser.pavel@gmail.com>
2019-08-08 19:09:03 +0800

    added show post route, controller action and jbuilder

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :posts, only: [ :index, :create ]
+      resources :posts, only: [ :index, :create, :show ]
       resources :comments, only: [ :create ]
     end
   end
